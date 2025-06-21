252f6542ac75f20ff39649f4c57a8fb481e856ab
Shubhamjoshi375 <joshishubham375@gmail.com>
2025-06-21 15:47:17 +0530

    third

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import Navbar from "@/components/ui/Navbar"
-import Footer from "@/components/ui/Footer"
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,9 +14,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body >
-        {/* <Navbar /> */}
-        {children}
-        {/* <Footer /> */}
+             {children}
+       
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 import HeroSection from "@/components/ui/HeroSection"
-import FeatureSection from "@/components/ui/FeatureSection"
-import TrustedSection from "@/components/ui/TrustedSection"
-import CTASection from "@/components/ui/CTASection"
+
 import About from "@/components/ui/About"
-import BannerCTA from "@/components/ui/BannerCTA"
-import Partners from "@/components/ui/Partners"
-import Cards from "@/components/ui/Cards"
-import DarkCTA from "@/components/ui/DarkCTA"
-import QuoteForm from "@/components/ui/QuoteForm"
-import AppPromo from "@/components/ui/AppPromo"
-import Gallery from "@/components/ui/Gallery"
+
 import FeatureCards from "@/components/ui/FeatureCards"
-import Testimonial from "@/components/ui/Testimonial"
-import FAQ from "@/components/ui/FAQ"
-import Footer from "@/components/ui/Footer"
+
 import InfoSection from "@/components/ui/section"
 
 
@@ -25,19 +15,7 @@ export default function Home() {
 
      <InfoSection/> <FeatureCards />
 
-      {/* <BannerCTA />
-      <Partners />
-      <Cards />
-           <FeatureSection />
-      <DarkCTA />
-      <QuoteForm />
-      <AppPromo />
-      <Gallery />
-     
-      <Testimonial />
-      <CTASection />
-      <FAQ />
-      <Footer /> */}
+      
     </main>
   )
 }
