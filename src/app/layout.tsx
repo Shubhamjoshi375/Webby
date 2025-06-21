12e04304d9d4007631ff707c60409006fc70f7cf
Shubhamjoshi375 <joshishubham375@gmail.com>
2025-06-21 15:53:25 +0530

    Fix: Removed unused variable 'inter' or applied it to <body>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body >
+      <body className={inter.className}>
+        
              {children}
        
       </body>
